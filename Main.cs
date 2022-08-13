625eb0c8a4e84bd0268f79bc02b253f25aaa3752
Administrator <Administrator@DESKTOP-KSL6APH>
2022-08-14 00:31:35 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBSHAGA
+{
+    class Run
+    {
+
+        static void Main(string[] args)
+        {
+            qGame myGame = new qGame();
+            myGame.RunMainMenu();
+
+            string prompt = "Начать с начала?\n";
+            List<string> options = new List<string>()
+            {
+                "В главное меню",
+                "Новая игра",
+                "Продолжить",
+                "Выйти из игры"
+            };
+
+            Menu mainMenu = new Menu(prompt, options, 101);
+            int selectedIndex = mainMenu.Run();
+
+            do
+            {
+                switch (options[selectedIndex])
+                {
+                    case "В главное меню":
+                        myGame.RunMainMenu();
+                        selectedIndex = mainMenu.Run();
+                        break;
+
+                    case "Новая игра":
+                        myGame.NewGame();
+                        selectedIndex = mainMenu.Run();
+                        break;
+
+                    case "Продолжить":
+                        myGame.Resume();
+                        selectedIndex = mainMenu.Run();
+                        break;
+                }
+            }
+            while (options[selectedIndex] != "Выйти из игры");
+        }
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBSHAGA
+{
+    class Menu
+    {
+        private int SelectedIndex; //Номер пункта
+        private List<string> Options; //Строка с выбором нужного пункта
+        private string Prompt; //Строка Меню
+        private int Health; // Кол-во HP
+
+        public Menu(string prompt, List<string> options, int health)
+        {
+            Prompt = prompt;
+            Health = health;
+            Options = options;
+            SelectedIndex = 0;
+
+        }
+
+        private void DisplayOptions() // Визуализация движения курсора
+        {
+            if (Health == 101) Console.Write(Prompt + "\n\n");
+            else
+            {
+                Console.Write("\nЗдоровье: ");
+                if (Health >= 50) Console.ForegroundColor = ConsoleColor.Green;
+                else if (Health < 50 && Health >= 30) Console.ForegroundColor = ConsoleColor.Yellow;
+                else if (Health < 30) Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(Health + "\n\n");
+                Console.ResetColor();
+                Console.Write(Prompt + "\n\n");
+            }
+
+            for (int i = 0; i < Options.Count; i++)
+            {
+                string currentOption = Options[i];
+                string prefix;
+                if (i == SelectedIndex)
+                {
+                    prefix = "*";
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    prefix = " ";
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine($"{prefix} << {currentOption} >>");
+            }
+            Console.ResetColor();
+        }
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Console.Clear();
+                DisplayOptions();
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                //Обновление курсора
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = Options.Count - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex == Options.Count)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+            }
+            while (keyPressed != ConsoleKey.Enter);
+
+            return SelectedIndex;
+
+        }
+    }
+}
